aa2599fc3514a8b542aee4a821d2307c23ef223f
sulthanrps <sulthanrafips@gmail.com>
2026-06-06 19:05:09 +0700

    feat: deployment preparation

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import supabase
-
+from flask_cors import CORS
 from routes.mind_check import mind_check_bp
 from routes.auth import auth_bp
 from routes.profile import profile_bp
@@ -9,7 +9,7 @@ from routes.journal import journal_bp
 from routes.schedule import schedule_bp
 
 app = Flask(__name__)
-
+CORS(app)
 app.register_blueprint(mind_check_bp, url_prefix='/mind-checks')
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(profile_bp, url_prefix='/profile')
